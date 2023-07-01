df0d06f70ef486b9f92434793a694ac92e48c59f
cpmooney <christopher.mooney.math@gmail.com>
2023-07-01 14:46:50 -0500

    Version without trying to connect to neon

--- server/api/data.get.ts
+++ server/api/data.get.ts
@@ -1,14 +1,3 @@
-import { createPool } from '@vercel/postgres';
-
-const postgresUrl = useRuntimeConfig().POSTGRES_URL;
-
-const pool = createPool({
-  connectionString: postgresUrl,
-});
-
 export default defineEventHandler(async (event) => {
-  const users = await pool.sql`SELECT * FROM users;`;
-  return {
-    users: users.rows,
-  };
+  return 'without a database';
 });
